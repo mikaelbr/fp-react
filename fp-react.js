ab348cd2b573766af89967ab108b02be9750804e
Mikael Brevik <mikaelbre@gmail.com>
2016-05-05 16:15:50 +0200

    Exposes new functions

--- fp-react.js
+++ fp-react.js
@@ -2,9 +2,15 @@
 import memoizeish from './lib/memoizeish';
 import partial from './lib/partial';
 import partialRight from './lib/partialRight';
+import curry from './lib/curry';
+import curryRight from './lib/curryRight';
+import wrap from './lib/wrap';
 
 export {
   memoizeish,
   partial,
-  partialRight
+  partialRight,
+  curry,
+  curryRight,
+  wrap
 };
