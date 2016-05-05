30c847d6b94d5e258ae23a2400b64b6e82695c50
Mikael Brevik <mikaelbre@gmail.com>
2016-05-05 14:37:15 +0200

    Exports partialRight to public API

--- fp-react.js
+++ fp-react.js
@@ -1,8 +1,10 @@
 
 import memoizeish from './lib/memoizeish';
 import partial from './lib/partial';
+import partialRight from './lib/partialRight';
 
 export {
   memoizeish,
-  partial
+  partial,
+  partialRight
 };
